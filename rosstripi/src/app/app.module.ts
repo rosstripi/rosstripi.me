a629dd97ee6cffcf9f84a464669ce2bf7f14e86a
Ross Tripi <ross@mandarincreativegroup.com>
2017-08-01 02:27:58 -0400

    Work on a contact page that implements Firebase database storage
    Also ignore environment.ts because it contains API keys

--- rosstripi/src/app/app.module.ts
+++ rosstripi/src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -23,7 +30,10 @@ import { FooterComponent } from './footer/footer.component';
       { path: 'contact', component: ContactComponent },
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       { path: '**', redirectTo: '/home', pathMatch: 'full' }
-    ])
+    ]),
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase, 'rosstripi'),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- rosstripi/src/app/contact/contact.component.ts
+++ rosstripi/src/app/contact/contact.component.ts
@@ -1,3 +1,6 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireDatabase } from 'angularfire2/database';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private db: AngularFireDatabase) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    const {name, email, message} = this.form.value;
+    const date = Date();
+    const html = `
+      <div>From: ${name}</div>
+      <div>Email: <a href="mailto:${email}">${email}</a></div>
+      <div>Date: ${date}</div>
+      <div>Message: ${message}</div>
+    `;
+    let formRequest = { name, email, message, date, html };
+    this.db.list('/messages').push(formRequest);
+    this.form.reset();
+  }
 
   ngOnInit() {
   }
